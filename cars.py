0dc3af31c7678a3c298416bb2e3e6701c216e0a2
djibril soukouna <djib_cisse_9@hotmail.com>
2025-09-17 12:12:55 +0200

    update classes

--- cars.py
+++ cars.py
@@ -1,5 +1,7 @@
 class Car:
   runs = True
+  number_of_wheesl = 4
+  accident_subits = 2
 
   def __init__(self, name):
     print("New car!")
@@ -10,4 +12,13 @@ class Car:
       print(f"{name.capitalize()} car is started")
     else:
       print(f"{name.capitalize()} car is broken! :(")
+
+  @classmethod
+  def get_number_of_crashes(cls):
+    return cls.accident_subits
+
     
+c5 = Car("citro")
+c5.start("citroo")
+print(c5.accident_subits)
+
